6548bdbf822fa11ad169ab5a246819a4143c154e
Yassine <Oussiyassine1@gmail.com>
2023-02-28 00:14:33 +0100

    colloborateur Controller commit

--- src/main/java/com/gestion/rh/controller/CollaborateurController.java
+++ src/main/java/com/gestion/rh/controller/CollaborateurController.java
@@ -1,22 +1,48 @@
 package com.gestion.rh.controller;
 
 import com.gestion.rh.models.Collaborateur;
+import com.gestion.rh.service.collaborateurService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/collaborateur")
 @RequiredArgsConstructor
 public class CollaborateurController {
 
-    @PostMapping("/")
-    ResponseEntity<String> addUser(@Valid @RequestBody Collaborateur collaborateur) {
-        // persisting the user
-        return ResponseEntity.ok("User is valid");
+    @Autowired
+    private collaborateurService collaborateurService;
+    @PostMapping()
+    public Collaborateur saveUser(@RequestBody Collaborateur collaborateur) {
+        return collaborateurService.save(collaborateur);
     }
+
+
+    @GetMapping
+    public ResponseEntity<List<Collaborateur>> getAllSubreddits() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(collaborateurService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Collaborateur> getSubreddit(@PathVariable Long id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(collaborateurService.getCollaborateur(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Collaborateur> updateUser(@PathVariable("id") Long id,
+                                           @RequestBody Collaborateur collaborateur) {
+        collaborateur = collaborateurService.updateCollaborateur(id,collaborateur);
+        return ResponseEntity.ok(collaborateur);
+    }
+
 }
--- src/main/java/com/gestion/rh/service/collaborateurService.java
+++ src/main/java/com/gestion/rh/service/collaborateurService.java
@@ -1,12 +1,17 @@
 package com.gestion.rh.service;
 
 
+import com.gestion.rh.exceptions.RhException;
 import com.gestion.rh.models.Collaborateur;
 import com.gestion.rh.models.NotificationEmail;
 import com.gestion.rh.repository.CollaborateurRepository;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
@@ -22,4 +27,28 @@ public class collaborateurService {
                 "http://localhost:8081/" ));
         return save;
     }
+    @Transactional(readOnly = true)
+    public List<Collaborateur> getAll() {
+        return collaborateurRepository.findAll()
+                .stream()
+                .collect(toList());
+    }
+
+    public Collaborateur getCollaborateur(Long id) {
+        Collaborateur collaborateur = collaborateurRepository.findById(id)
+                .orElseThrow(() -> new RhException("No collaborateur found with ID - " + id));
+        return collaborateur;
+    }
+
+    public Collaborateur updateCollaborateur(Long id, Collaborateur collaborateur) {
+        Collaborateur collaborateurAncien =
+                collaborateurRepository.findById(id).get();
+        collaborateurAncien.setSalaire(collaborateur.getSalaire());
+        collaborateurAncien.setPoste_App(collaborateur.getPoste_App());
+        collaborateurAncien.setManager_rh(collaborateur.getManager_rh());
+
+        collaborateurRepository.save(collaborateurAncien);
+        return collaborateurAncien;
+    }
+
 }
